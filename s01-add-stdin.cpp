86b49a40630d5e55ac0ecd99023217002e6be22a
s24265 <91980319+s24265@users.noreply.github.com>
2021-10-13 23:40:16 +0200

    Update s01-add-stdin.cpp

--- s01-add-stdin.cpp
+++ s01-add-stdin.cpp
@@ -13,7 +13,7 @@ auto main() -> int
 {
     auto const a = ask_user_for_integer("a = ");
     auto const b = ask_user_for_integer("b = ");
-    std::cout << (a + b) << "\n"
+    std::cout << (a + b) << "\n";
     return 0;
 }
 
