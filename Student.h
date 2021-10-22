563f4b781955450bbe6ec7da6ec2fdff709246ca
s24265 <91980319+s24265@users.noreply.github.com>
2021-10-22 11:18:31 +0200

    Add files via upload

---
+++ Student.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+struct Student
+{
+    std::string imie;
+    std::string nazwisko;
+    std::string indeks;
+    short int semsetr;
+    int srednia;
+    
+    
+};
