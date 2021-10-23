b4438a3d55f60957696130f141619258ac3ff4be
s24265 <91980319+s24265@users.noreply.github.com>
2021-10-23 20:08:39 +0200

    Add files via upload

---
+++ w3/Student.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+struct Student
+{
+    std::string imie;
+    std::string nazwisko;
+    std::string indeks;
+    short int semsetr;
+    int srednia;
+    
+    
+};
+
