c58de4987edff2c4db105882cfb9509ae24ce335
s24265 <91980319+s24265@users.noreply.github.com>
2021-10-29 09:36:26 +0200

    Update s03-password.cpp

--- w2/s03-password.cpp
+++ w2/s03-password.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
 #include <string>
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::string password = "student";
+    std::string password = argv[1];
     std::string x = "";
 
     do
     {
         std::cout << "Password: ";
         std::getline(std::cin, x);
-    }
-    
+    }    
     while (password != x);
     std::cout <<"ok!\n";
 }
-
